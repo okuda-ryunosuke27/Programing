188e134449bb4e06ac063de31dbcb198b5114710
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2024-01-25 11:10:52 +0900

    オブジェクトが終わりシーンへ
    シーンはまだヘッダーを書いている

--- Game/DriveAndAvoid/Scene/HelpScene.h
+++ Game/DriveAndAvoid/Scene/HelpScene.h
@@ -1,5 +1,13 @@
 #pragma once
+
+#include "SceneBase.h"
+
 class HelpScene
 {
+private:
+	int background_image;		//”wŒi‰æ‘œ
+
+public:
+
 };
 
--- Game/DriveAndAvoid/Utility/InputControl.h
+++ Game/DriveAndAvoid/Utility/InputControl.h
@@ -1,19 +1,32 @@
 #pragma once
-#include "InputControl.h"
+#include "Vector2D.h"
 
 class InputControl
 {
-private:
-	static bool now_button[16];
-	static bool old_button[16];
-	static float trigger[2];
-	static Vector2D stick[2];
+	private:
+		static bool now_button[16];
+		static bool old_button[16];
+		static float trigger[2];
+		static Vector2D stick[2];
 
-public:
-	static void Update();
+	public:
+		static void Update();						//更新処理
 
-	//ボタン入力取得処理
-	static bool GetButton(int button);
-	static bool GetButtonDown(int button);
+		//ボタン入力取得処理
+		static bool GetButton(int button);			//押し続けている間
+		static bool GetButtonDown(int button);		//押した瞬間
+		static bool GetButtonUp(int button);		//離した瞬間
+
+		//トリガー入力取得処理
+		static float GetLeftTrigger();				//左トリガー
+		static float GetRightTrigger();				//右トリガー
+
+		//スティック入力取得処理
+		static Vector2D GetLeftStick();				//左スティック
+		static Vector2D GetRightStick();			//右スティック
+
+	private:
+		//ボタン配列範囲チェック
+	static bool CheckButtonRange(int button);
 };
 
