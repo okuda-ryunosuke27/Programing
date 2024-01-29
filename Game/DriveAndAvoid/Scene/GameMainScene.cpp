2c6ef0a301aef3df1f9ff9e95865136c05493aaa
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2024-01-29 15:00:55 +0900

    シーンのヘッダーは終わった
    今はゲームMainを書いている

--- Game/DriveAndAvoid/Scene/GameMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -1,1 +1,29 @@
 #include "GameMainScene.h"
+#include "../Object/RankingData.h"
+#include "DxLib.h"
+#include <math.h>
+
+GameMainScene::GameMainScene() :high_score(0),
+back_ground(NULL), barrier_image(NULL), mileage(0), player(nullptr),
+enemy(nullptr)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		enemy_image[i] = NULL;
+		enemy_count[i] = NULL;
+	}
+}
+
+GameMainScene::~GameMainScene() {	}
+
+//‰Šú‰»ˆ—
+void GameMainScene::Initialize()
+{
+	//‚“¾“_’l‚ð“Ç‚Ýž‚Þ
+	ReadHighScore();
+
+	//‰æ‘œ‚Ì“Ç‚Ýž‚Ý
+	back_ground = LoadGraph("Resource/images/back.bmp");
+	barrier_image = LoadGraph("Reasource/images/barrier.png");
+	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120, enemy_image);
+}
--- Game/DriveAndAvoid/Scene/HelpScene.h
+++ Game/DriveAndAvoid/Scene/HelpScene.h
@@ -2,12 +2,21 @@
 
 #include "SceneBase.h"
 
-class HelpScene
+class HelpScene : public SceneBase
 {
-private:
-	int background_image;		//”wŒi‰æ‘œ
+	private:
+		int background_image;		//”wŒi‰æ‘œ
 
-public:
+	public:
+		HelpScene();
+		virtual ~HelpScene();
+
+		virtual void Initialize() override;
+		virtual eSceneType Update() override;
+		virtual void Draw() const override;
+		virtual void Finalize() override;
+
+		virtual eSceneType GetNowScene() const override;
 
 };
 
