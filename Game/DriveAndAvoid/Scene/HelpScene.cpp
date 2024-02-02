9d99cc862317255748bb27883160290bcc9d2b85
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2024-02-02 15:00:44 +0900

    シーン書いてる途中
    ゲームメインが書けたのでヘルプを書いている途中

--- Game/DriveAndAvoid/Scene/HelpScene.cpp
+++ Game/DriveAndAvoid/Scene/HelpScene.cpp
@@ -1,1 +1,39 @@
 #include "HelpScene.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+HelpScene::HelpScene() :background_image(NULL)
+{
+
+}
+
+HelpScene::~HelpScene()
+{
+
+}
+
+//初期化処理
+void HelpScene::Initialize()
+{
+	//画像の読み込み
+	background_image = LoadGraph("Resource/images/Title.bmp");
+
+	//エラーチェック
+	if (background_image == -1)
+	{
+		throw("Resource/images/Title.bmpがありません\n");
+	}
+}
+
+//更新処理
+eSceneType HelpScene::Update()
+{
+	//Bボタンが押されたら、タイトルに戻る
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B))
+	{
+		return eSceneType::E_TITLE;
+	}
+
+	return GetNowScene();
+}
+
