2f2c5f9ed9abb93a803b0f7409b6e2bf45acf863
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2024-02-06 19:03:43 +0900

    ランキングインプット
    まだ途中

--- Game/DriveAndAvoid/Scene/RankingDispScene.cpp
+++ Game/DriveAndAvoid/Scene/RankingDispScene.cpp
@@ -32,5 +32,41 @@ void RankingDispScene::Initialize()
 //更新処理
 eSceneType RankingDispScene::Update()
 {
-	//
+	//Bボタンが押されたら、タイトルに戻る
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B))
+	{
+		return eSceneType::E_TITLE;
+	}
+
+	return GetNowScene();
+}
+
+//描画処理
+void RankingDispScene::Draw() const
+{
+	//背景画像の描画
+	DrawGraph(0, 0, background_image, FALSE);
+
+	//取得したランキングデータを描画する
+	for (int i = 0; i < 5; i++)
+	{
+		DrawFormatString(50, 170 + i * 25, 0xFFFFFF, "%2d %-15s %6d", ranking->GetRank(i), ranking->GetName(i), ranking->GetScore(i));
+	}
+}
+
+//終了時処理
+void RankingDispScene::Finalize()
+{
+	//読み込んだ画像の削除
+	DeleteGraph(background_image);
+
+	//動的メモリの解放
+	ranking->Finalize();
+	delete ranking;
+}
+
+//現在のシーン情報を取得
+eSceneType RankingDispScene::GetNowScene() const
+{
+	return eSceneType::E_RANKING_DISP;
 }
