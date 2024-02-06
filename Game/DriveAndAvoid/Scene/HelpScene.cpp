2b30ce50c240717440e9e8af78dda2a0ebc3fe88
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2024-02-06 14:59:22 +0900

    ランキングdispを書いている

--- Game/DriveAndAvoid/Scene/HelpScene.cpp
+++ Game/DriveAndAvoid/Scene/HelpScene.cpp
@@ -37,3 +37,33 @@ eSceneType HelpScene::Update()
 	return GetNowScene();
 }
 
+//描画処理
+void HelpScene::Draw() const
+{
+	//背景画像の描画
+	DrawGraph(0, 0, background_image, FALSE);
+
+	//ゲーム説明
+	SetFontSize(16);
+	DrawString(20, 120, "ヘルプ画面", 0xFFFFFF, 0);
+
+	DrawString(20, 160, "これは障害物を避けながら", 0xFFFFFF, 0);
+	DrawString(20, 180, "走り続けるゲームです。", 0xFFFFFF, 0);
+	DrawString(20, 200, "燃料が尽きるか障害物に", 0xFFFFFF, 0);
+	DrawString(20, 220, "数回当たるとゲームオーバーです。", 0xFFFFFF, 0);
+
+	DrawString(150, 450, "---- Bボタンを押してタイトルへ戻る ----", 0xFFFFFF, 0);
+}
+
+//終了時処理
+void HelpScene::Finalize()
+{
+	//読み込んだ画像の削除
+	DeleteGraph(background_image);
+}
+
+//現在のシーン情報を取得
+eSceneType HelpScene::GetNowScene() const
+{
+	return eSceneType::E_HELP;
+}
--- Game/DriveAndAvoid/Scene/RankingDispScene.cpp
+++ Game/DriveAndAvoid/Scene/RankingDispScene.cpp
@@ -1,1 +1,36 @@
 #include "RankingDispScene.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+RankingDispScene::RankingDispScene() :background_image(NULL), ranking(nullptr)
+{
+
+}
+
+RankingDispScene::~RankingDispScene()
+{
+
+}
+
+//初期化処理
+void RankingDispScene::Initialize()
+{
+	//画面の読み込み
+	background_image = LoadGraph("Resource/images/Ranking.bmp");
+
+	//エラーチェック
+	if (background_image == -1)
+	{
+		throw("Resource/images/Ranking.bmpがありません\n");
+	}
+
+	//ランキング情報を取得
+	ranking = new RankingData;
+	ranking->Initialize();
+}
+
+//更新処理
+eSceneType RankingDispScene::Update()
+{
+	//
+}
